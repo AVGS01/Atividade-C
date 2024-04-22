358991c7332b2b95f2ef7cb02ec2e9b8eb3eeae4
André Vitor Guimarães e Silva <129067726+AVGS01@users.noreply.github.com>
2024-04-22 19:39:33 -0300

    Projeto.c

--- atividade.c
+++ atividade.c
@@ -9,23 +9,28 @@ struct Aluno {
     float nota2;
 };
 
+int lerAluno(struct Aluno *aluno, FILE *entrada);
+float calcularMedia(float nota1, float nota2);
+const char* definirSituacao(float media);
+
 int main() {
     FILE* entrada;
     FILE* saida;
     struct Aluno aluno;
+
     entrada = fopen("../DadosEntrada.csv", "r");
     saida = fopen("SituacaoFinal.csv", "w");
 
     if (entrada == NULL || saida == NULL) {
-        printf("Arquivos não encontrados!");
+        printf("Erro ao abrir os arquivos!");
         exit(1);
     }
 
     fprintf(saida, "NOME, NOTA, SITUACAO\n");
 
-    while (fscanf(entrada, "%[^,],%[^,],%[^,],%f,%f\n", aluno.nome, aluno.tel, aluno.curso, &aluno.nota1, &aluno.nota2) != EOF) {
-        float media = (aluno.nota1 + aluno.nota2) / 2.0;
-        const char* situacao = (media >= 7) ? "APROVADO" : "REPROVADO";
+    while (lerAluno(&aluno, entrada) != EOF) {
+        float media = calcularMedia(aluno.nota1, aluno.nota2);
+        const char* situacao = definirSituacao(media);
         fprintf(saida, "%s, %.2f, %s\n", aluno.nome, media, situacao);
     }
 
@@ -33,4 +38,22 @@ int main() {
     fclose(saida);
 
     return 0;
+}
+
+int lerAluno(struct Aluno *aluno, FILE *entrada) {
+    char linha[256];
+    if (fgets(linha, sizeof(linha), entrada) == NULL)
+        return EOF;
+
+    sscanf(linha, "%[^,],%[^,],%[^,],%f,%f", aluno->nome, aluno->tel, aluno->curso, &aluno->nota1, &aluno->nota2);
+    
+    return 1;
+}
+
+float calcularMedia(float nota1, float nota2) {
+    return (nota1 + nota2) / 2.0;
+}
+
+const char* definirSituacao(float media) {
+    return (media >= 7) ? "APROVADO" : "REPROVADO";
 }
